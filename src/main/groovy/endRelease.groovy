b2aeeaae5cdef9592d894d011b61dd4d1235efc9
Vatsalya Singh <vatsalya>
2021-07-19 17:47:53 +0530

    refactoring

---
+++ src/main/groovy/endRelease.groovy
@@ -0,0 +1,31 @@
+def endRelease(String source, String target, boolean debugmode,def gitOperation, def buildVersion){
+    deleteDir();
+    echo "===================== Checking out " + gitRepo + " ====================="
+    gitOperation.gitCheckout(gitRepo, 'dev') // TODO: pick dev branch from jgit configuration
+
+    def releaseBranchExists = gitOperation.isReleaseBranchExists()
+    if (releaseBranchExists == true) {
+        sh 'git branch'
+        sh 'git checkout dev'
+        sh 'git branch -a'
+        sh 'git branch -r'
+        echo "Run maven command for ending the release"
+
+        echo "Debug mode" + debugmode
+        if (!debugmode) {
+            withMaven(maven: 'JGIT_PIPELINE_MAVEN_PLUGIN') {
+
+                withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'JGIT_PIPELINE_TARGET_REPOS_CREDS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+                    sh "mvn jgitflow:release-finish -DallowSnapshots -DperformRelease=false -DpushReleases=true -Dusername=$USERNAME -Dpassword=$PASSWORD"
+                    // def output = sh returnStdout: true, script: "mvn jgitflow:release-finish -DallowSnapshots -DperformRelease=false -DpushReleases=true -Dusername=root -Dpassword=Passw0rd"
+                    //echo output
+                }
+            }
+        }
+    } else {
+        echo "No release branch exist for " + gitRepo
+    }
+
+    echo "cleaning workspace"
+    deleteDir();
+}
